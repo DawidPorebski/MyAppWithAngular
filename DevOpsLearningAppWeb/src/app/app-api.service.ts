7e75d1cf2f7c45c859ba6e9d1991a3dfd9a673a8
Dawid Porebski <mever12@gmail.com>
2022-05-24 21:10:54 +0200

    Changed api services file

--- DevOpsLearningAppWeb/src/app/app-api.service.ts
+++ DevOpsLearningAppWeb/src/app/app-api.service.ts
@@ -7,27 +7,38 @@ import { environment } from "../environments/environment";
   providedIn: 'root'
 })
 export class AppApiService {
+  private readonly _headerDict = {
+    "Access-Control-Allow-Origin": "http://3.72.251.204:5006"
+  }
+
   public constructor(
     private readonly _http: HttpClient) {
   }
 
   public async getRows(): Promise<MyEntityViewModel[]> {
-    const observable = await this._http.get<MyEntityViewModel[]>(this._createUrl('rows'));
+    const observable = await this._http.get<MyEntityViewModel[]>(
+      this._createUrl('rows'), {
+        headers: this._headerDict
+      });
     const response = await observable.toPromise();
     return (response ?? []).map(vm => new MyEntityViewModel(vm));
   }
 
   public async addRow(): Promise<void> {
-    const observable = await this._http.post(this._createUrl("add-row"), null);
+    const observable = await this._http.post(this._createUrl("add-row"), null, {
+      headers: this._headerDict
+    });
     await observable.toPromise();
   }
 
   public async removeAllRows(): Promise<void> {
-    const observable = await this._http.post(this._createUrl("remove-all-rows"), null);
+    const observable = await this._http.post(this._createUrl("remove-all-rows"), null, {
+      headers: this._headerDict
+    });
     await observable.toPromise();
   }
 
   private _createUrl(path: string): string {
-    return `${environment.backendBaseUrl}/${path}`
+    return ${environment.backendBaseUrl}/${path}
   }
 }
